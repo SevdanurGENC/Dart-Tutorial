dd738727d3a980d2f20c29faf67f29db0bff5e74
Sevdanur GENC <sevdanurgenc@gmail.com>
2020-11-23 19:42:46 +0300

    Create Dart04.dart

---
+++ Lectures/Dart04.dart
@@ -0,0 +1,15 @@
+void main() { 
+  //fonksiyonlar : 
+  
+//   DonenVeriTipi fonksiyonAdi(gonderilenDeger){
+//   //fonksiyon bilgisi
+//   }
+  
+  final retrievedName = getName('Sevdanur', 37);  
+  print(retrievedName); 
+  
+}
+
+ String getName(String name, int age){
+    return 'My name is : $name and my age is : $age';
+ }
